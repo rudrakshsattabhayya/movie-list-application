7f0c59b70429a4a6cb4edfb01938172e5135c2d9
Rudraksh Sattabhayya <rudrakshsattabhayya@gmail.com>
2023-07-23 19:17:56 +0530

    pushing changes

--- page.js
+++ page.js
@@ -2,7 +2,10 @@ var apiUrl = "https://www.omdbapi.com/?apikey=3c056857";
 var moviesList = [];
 var numOfPages = 1;
 var pageNum = 1;
-var searchKeyword=""
+var movieDetails = {};
+var searchKeyword="";
+var commentText = "";
+var starRating = 0;
 const loader = `<div class="spinner">
   <div class="bounce1"></div>
   <div class="bounce2"></div>
@@ -28,6 +31,41 @@ const movieListPageBottomHTML = `
         </div>
 `;
 
+const starInput = `
+<div class="rating">
+<label>
+  <input type="radio" name="stars" value="1" />
+  <span class="icon">★</span>
+</label>
+<label>
+  <input type="radio" name="stars" value="2" />
+  <span class="icon">★</span>
+  <span class="icon">★</span>
+</label>
+<label>
+  <input type="radio" name="stars" value="3" />
+  <span class="icon">★</span>
+  <span class="icon">★</span>
+  <span class="icon">★</span>   
+</label>
+<label>
+  <input type="radio" name="stars" value="4" />
+  <span class="icon">★</span>
+  <span class="icon">★</span>
+  <span class="icon">★</span>
+  <span class="icon">★</span>
+</label>
+<label>
+  <input type="radio" name="stars" value="5" />
+  <span class="icon">★</span>
+  <span class="icon">★</span>
+  <span class="icon">★</span>
+  <span class="icon">★</span>
+  <span class="icon">★</span>
+</label>
+</div>
+`
+
 const fetchMovies = async (keyword, pageNumber) => {
     await fetch(`${apiUrl}&s=${keyword}&page=${pageNumber}`).then((res) => {
         let obj = res.json();
@@ -41,6 +79,40 @@ const fetchMovies = async (keyword, pageNumber) => {
             numOfPages = pages;
             localStorage.setItem("moviesList", JSON.stringify(movies));
             localStorage.setItem("numOfPages", JSON.stringify(pages));
+        }else{
+            localStorage.setItem("errorStatus", JSON.stringify(false))
+            localStorage.setItem("errorMsg", JSON.stringify(res.Error))
+        }
+        
+    });
+}
+
+const fetchMovieDetails = async (id) => {
+    await fetch(`${apiUrl}&i=${id}`).then((res) => {
+        let obj = res.json();
+        return obj;
+    }).then((res) => {
+        if(res.Response === "True"){
+            localStorage.setItem("errorStatus", JSON.stringify(false));
+
+            movieDetails = {
+                Title: res.Title,
+                Poster: res.Poster,
+                Released: res.Released,
+                Runtime: res.Runtime,
+                Plot: res.Plot,
+                Genre: res.Genre,
+                Director: res.Director,
+                Writer: res.Writer,
+                Actors: res.Actors,
+                Language: res.Language,
+                Country: res.Country,
+                Awards: res.Awards,
+                imdbRating: res.imdbRating,
+                BoxOffice: res.BoxOffice
+            }
+
+            localStorage.setItem("movieDetails", JSON.stringify(movieDetails));
         }else{
             localStorage.setItem("errorStatus", JSON.stringify(false))
             localStorage.setItem("errorMsg", JSON.stringify(res.Error))
@@ -65,17 +137,30 @@ const updatePageNumber = async (event) => {
 }
 
 const handleEnterInSeachbar = async (event) => {
-
     if (event.keyCode === 13) {
         handleEditInSearchBar(event);
     }
   };
 
+const handleComment = (event) => {
+    let text = event.target.value;
+    commentText = text;
+    localStorage.setItem("commentText", commentText);
+}
+
+const viewMovieDetails = async (event) => {
+    document.getElementById("mainContainer").innerHTML = loader;
+    const index = parseInt(event.target.id);
+    let movies = JSON.parse(localStorage.getItem("moviesList"));
+    let imdbID = movies[index].imdbID;
+    await fetchMovieDetails(imdbID);
+    updateMovieDetailsHTML();
+}
+
 const addFunctionalities = () => {
     const handleEditInSearchBarInput = document.getElementById("searchbarInput");
     handleEditInSearchBarInput.addEventListener("input", handleEditInSearchBar);
     handleEditInSearchBarInput.addEventListener("keydown", handleEnterInSeachbar);
-
 
     const searchButton = document.getElementById("searchKeyword");
     searchButton.addEventListener("click", searchMovies);
@@ -84,6 +169,24 @@ const addFunctionalities = () => {
     for(let elem of changePage){
         elem.addEventListener("click", updatePageNumber);
     }
+    
+    const movieItems = document.getElementsByClassName("movieListItem");
+    for(let movie of movieItems){
+        movie.addEventListener("click", viewMovieDetails); 
+    };
+}
+
+const addFunctionalitiesForMovieDetails = () => {
+    const radioInputs = document.querySelectorAll('input[type="radio"]');
+
+  radioInputs.forEach(function(radio) {
+    radio.addEventListener('change', function() {
+      console.log('New star rating: ' + this.value);
+    });
+  });
+
+  const addComment = document.getElementById("commentInput");
+  addComment.addEventListener("input", handleComment);
 }
 
 const updatePagination = () => {
@@ -124,8 +227,8 @@ const updateMoviesListHTML = () => {
         let poster = movie.Poster;
         movieListItems += `
         <div class="movieListItem" id="${index}">
-                    <img alt="poster" src="${poster}" class="movieListPoster" />
-                    <div class="movieListTitle">${truncateTitle}</div>
+                    <img id="${index}" alt="poster" src="${poster}" class="movieListPoster" />
+                    <div id="${index}" class="movieListTitle">${truncateTitle}</div>
         </div>
         `
     });
@@ -155,11 +258,70 @@ const intitialize = async () => {
     }else{
         searchKeyword = localStorage.getItem("searchKeyword");
     }
+    if(!localStorage.getItem("commentText")){
+        localStorage.setItem("commentText", JSON.stringify(commentText));
+    }else{
+        commentText = localStorage.getItem("commentText");
+    }
+    if(!localStorage.getItem("starRating")){
+        localStorage.setItem("starRating", JSON.stringify(starRating));
+    }else{
+        starRating = JSON.parse(localStorage.getItem("starRating"));
+    }
+    if(!localStorage.getItem("movieDetails")){
+        localStorage.setItem("movieDetails", JSON.stringify(movieDetails));
+    }else{
+        movieDetails = JSON.parse(localStorage.getItem("movieDetails"));
+    }
     
     localStorage.setItem("pageNum", JSON.stringify(1));
     // await fetchMovies("marvel", 1);
     updateMoviesListHTML();
 
+}
+
+
+
+
+
+
+
+
+const updateMovieDetailsHTML = () => {
+    const upperHTML =`
+    <div id="movieDetailsMainContainer">
+            <div id="titleMovieDetails">${movieDetails.Title}</div>
+            <div id="movieDetailsSection">
+                <img src="${movieDetails.Poster}" />
+                <div id="movieDetails">`;
+    const lowerHTML = `
+    </div>
+            </div>
+            <div class="userInputMovieDetails" id="yourRating">
+                <label>Your Rating: </label>
+                ${starInput}
+            </div>
+            <div id="yourComment">
+                <label>Your Comment: </label>
+                <input id="commentInput" type="text-area" value="${commentText}" placeholder="Write your Review..." />
+            </div>
+        </div>`;
+
+    let movieDetailsItemsHTML = "";
+
+    for(let key in  movieDetails) {
+        if(key !== "Title" && key !== "Poster"){
+            movieDetailsItemsHTML += `
+            <div class="movieDetailsItem">
+                <div class="movieDetailsItemHeading">${key}:</div>
+                <div class="movieDetailsItemValue">${movieDetails[key]}</div>
+            </div>`
+        }
+    };
+
+    const totalHTML = upperHTML + movieDetailsItemsHTML + lowerHTML;
+    document.getElementById("mainContainer").innerHTML = totalHTML;
+    addFunctionalitiesForMovieDetails();
 }
 
 document.addEventListener("DOMContentLoaded", function () {
